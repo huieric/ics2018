e8fbda7b9fcb60c74c783374585628957ab41d08
tracer-ics2018 <tracer@njuics.org>
2018-12-14 14:28:19 +0000

    >  compile U201514582 shannonzhu Linux 3206989076e5 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux  14:28:19 up  3:50,  5 users,  load average: 0.07, 0.09, 0.02 acd173cf791e1ccdda5d360ecc80d733979ec7e2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -131,7 +131,7 @@ bool check_parentheses(const char* e, int p, int q) {
   return true;
 }
 
-uint32_t eval(const char* e, int p, int q) {
+uint32_t eval(const char* e, int p, int q, bool* success) {
   if (p > q) {
     Assert(0, "Bad expression");
     return 0;
@@ -145,19 +145,19 @@ uint32_t eval(const char* e, int p, int q) {
     Log("remove whitespace at %d and %d", p, q);
     while (e[p] == ' ') p++;
     while (e[q] == ' ') q--;
-    return eval(e, p, q);
+    return eval(e, p, q, success);
   }
   else if (check_parentheses(e, p, q) == true) {
     /* surrounded by a matched pair of parentheses */
     Log("remove a pair of parentheses at %d and %d", p, q);
-    return eval(e, p + 1, q - 1);
+    return eval(e, p + 1, q - 1, success);
   }
   else {
     int op = 0;
     for (int i = 0, pos = 0; i < nr_token; i++, pos += strlen(tokens[i].str)) {
       if (pos > q) {
 	/*Assert(0, "failed to find main operator");*/
-	panic("Illegal expression!");
+	*success = false;
 	return 0;
       }      
       if (pos < p) {
@@ -205,8 +205,8 @@ uint32_t eval(const char* e, int p, int q) {
     Log("main operator at %d", op);
 
     int op_type = e[op];
-    uint32_t val1 = eval(e, p, op - 1);
-    uint32_t val2 = eval(e, op + 1, q);
+    uint32_t val1 = eval(e, p, op - 1, success);
+    uint32_t val2 = eval(e, op + 1, q, success);
 
     switch (op_type) {
       case '+': return val1 + val2;
@@ -226,5 +226,5 @@ uint32_t expr(char *e, bool *success) {
   }
   
   *success = true;
-  return eval(e, 0, strlen(e) - 1);
+  return eval(e, 0, strlen(e) - 1, success);
 }
