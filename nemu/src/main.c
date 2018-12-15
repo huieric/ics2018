d27f000e89f531930469e8e0df11a37c205a5bc7
tracer-ics2018 <tracer@njuics.org>
2018-12-15 13:25:03 +0000

    >  compile U201514582 shannonzhu Linux 3206989076e5 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux  13:25:03 up 11:26,  5 users,  load average: 0.01, 0.01, 0.00 6bd49fbe9b5c2c965b438cc8d58a48da62a96bcb

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -9,23 +9,23 @@ void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
+  /*int is_batch_mode = init_monitor(argc, argv);*/
 
   /* Receive commands from user. */
-  ui_mainloop(is_batch_mode);
+  /*ui_mainloop(is_batch_mode);*/
   
-  /*FILE* fp = fopen("../tools/gen-expr/input", "r");*/
-  /*assert(fp != NULL);*/
-  /*int result;*/
-  /*while (fscanf(fp, "%u ", &result) != 0) {*/
-    /*char e[65536];*/
-    /*fgets(e, 65536, fp);*/
-    /*e[strlen(e) - 1] = 0;*/
-    /*bool success;*/
-    /*if (result != expr(e, &success)) {*/
-      /*printf("%u %s", result, e);*/
-    /*}*/
-  /*}*/
-  /*fclose(fp);*/
+  FILE* fp = fopen("../tools/gen-expr/input", "r");
+  assert(fp != NULL);
+  int result;
+  while (fscanf(fp, "%u ", &result) != 0) {
+    char e[65536];
+    fgets(e, 65536, fp);
+    e[strlen(e) - 1] = 0;
+    bool success;
+    if (result != expr(e, &success)) {
+      printf("%u %s", result, e);
+    }
+  }
+  fclose(fp);
   return 0;
 }
