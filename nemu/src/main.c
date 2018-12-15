88aa0839045d5344ae9495c950ccc3f100def862
tracer-ics2018 <tracer@njuics.org>
2018-12-15 13:34:05 +0000

    >  compile U201514582 shannonzhu Linux 3206989076e5 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux  13:34:05 up 11:35,  5 users,  load average: 0.04, 0.04, 0.01 5e5be6ffb3011041e70f835886a423bec199993

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -11,19 +11,21 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
   
-  FILE* fp = fopen("../tools/gen-expr/input", "r");
-  assert(fp != NULL);
+  FILE* fin = fopen("../tools/gen-expr/input", "r");
+  assert(fin != NULL);
   int result;
-  while (fscanf(fp, "%u ", &result) != 0) {
+  FILE* fout = fopen("./output", "w");
+  while (fscanf(fin, "%u ", &result) != 0) {
     char e[65536];
-    fgets(e, 65536, fp);
+    fgets(e, 65536, fin);
     e[strlen(e) - 1] = 0;
     bool success;
     if (result != expr(e, &success)) {
-      printf("%u %s", result, e);
+      fprintf(fout, "%u %s", result, e);
     }
   }
-  fclose(fp);
+  fclose(fin);
+  fclose(fout);
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
