3f7e22cbc59db07d383f48e8e01bb2d67663743f
tracer-ics2018 <tracer@njuics.org>
2018-12-16 02:28:10 +0000

    >  compile U201514582 shannonzhu Linux 3206989076e5 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux  02:28:10 up 18:52,  5 users,  load average: 0.38, 0.30, 0.12 4955f51cb42c1356325a081ad9450e546197da0

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -11,21 +11,21 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
   
-  /*FILE* fin = fopen("../tools/gen-expr/input", "r");*/
-  /*assert(fin != NULL);*/
-  /*int result;*/
-  /*FILE* fout = fopen("./output", "w");*/
-  /*while (fscanf(fin, "%u ", &result) != 0) {*/
-    /*char e[65536];*/
-    /*fgets(e, 65536, fin);*/
-    /*e[strlen(e) - 1] = 0;*/
-    /*bool success;*/
-    /*if (result != expr(e, &success)) {*/
-      /*fprintf(fout, "%u %s", result, e);*/
-    /*}*/
-  /*}*/
-  /*fclose(fin);*/
-  /*fclose(fout);*/
+  FILE* fin = fopen("../tools/gen-expr/input", "r");
+  assert(fin != NULL);
+  int result;
+  FILE* fout = fopen("./output", "w");
+  while (fscanf(fin, "%u ", &result) != 0) {
+    char e[65536];
+    fgets(e, 65536, fin);
+    e[strlen(e) - 1] = 0;
+    bool success;
+    if (result != expr(e, &success)) {
+      fprintf(fout, "%u %s", result, e);
+    }
+  }
+  fclose(fin);
+  fclose(fout);
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
