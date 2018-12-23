088cbbc062f837c3e227fd0a4d779b6eb991ddae
tracer-ics2018 <tracer@njuics.org>
2018-12-23 09:36:24 +0000

    >  compile U201514582 shannonzhu Linux 3206989076e5 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux  09:36:24 up 7 days,  3:04,  5 users,  load average: 0.00, 0.00, 0.00 c0b233d76baec8713a93d18998b9c2631823a3e2

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -162,13 +162,13 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   cpu.esp = cpu.esp - 4;
-  interpret_rtl_sm(&cpu.esp, src1, 4);
+  rtl_sm(&cpu.esp, src1, 4);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  interpret_rtl_lm(dest, &cpu.esp, 4);
+  rtl_lm(dest, &cpu.esp, 4);
   cpu.esp = cpu.esp + 4;
 }
 
