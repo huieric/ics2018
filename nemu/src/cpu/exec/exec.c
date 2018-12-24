e4c2f09b8a04a24a061618482fb0e034764a2c5a
tracer-ics2018 <tracer@njuics.org>
2018-12-24 12:54:45 +0000

    >  compile U201514582 shannonzhu Linux 3206989076e5 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux  12:54:45 up 8 days,  6:22,  5 users,  load average: 0.08, 0.03, 0.01 832b2e88b533566758499b97856ca1d5684eef

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -87,7 +87,7 @@ opcode_entry opcode_table [512] = {
   /* 0x30 */	EMPTY, IDEX(G2E, xor), EMPTY, IDEX(E2G, xor),
   /* 0x34 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x38 */	EMPTY, IDEX(G2E, cmp), EMPTY, IDEX(E2G, cmp),
-  /* 0x3c */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x3c */	EMPTY, IDEX(I, cmp), EMPTY, EMPTY,
   /* 0x40 */	IDEX(r, inc), IDEX(r, inc), IDEX(r, inc), IDEX(r, inc),
   /* 0x44 */	IDEX(r, inc), IDEX(r, inc), IDEX(r, inc), IDEX(r, inc),
   /* 0x48 */	IDEX(r, dec), IDEX(r, dec), IDEX(r, dec), IDEX(r, dec),
