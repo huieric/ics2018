24138f6e58e995d5d03c6bc666fae93a4fc7a08b
tracer-ics2018 <tracer@njuics.org>
2018-12-24 13:26:10 +0000

    >  compile U201514582 shannonzhu Linux 3206989076e5 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux  13:26:10 up 8 days,  6:54,  5 users,  load average: 0.03, 0.05, 0.01 ddbd54c3efd623b0e8bb3169cfdd3da780f38914

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -52,7 +52,7 @@ make_group(gp2,
   /* 0xf6, 0xf7 */
 make_group(gp3,
     IDEX(test_I, test), EMPTY, EX(not), EMPTY,
-    EMPTY, EMPTY, EMPTY, EX(idiv))
+    EMPTY, EX(imul1), EMPTY, EX(idiv))
 
   /* 0xfe */
 make_group(gp4,
