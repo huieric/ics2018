5faffb58e2a0c0d04b7a75d80a4a749e1e5aa1a0
tracer-ics2018 <tracer@njuics.org>
2018-12-24 05:53:29 +0000

    >  compile U201514582 shannonzhu Linux 3206989076e5 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux  05:53:29 up 7 days, 23:21,  5 users,  load average: 0.01, 0.04, 0.00 43b45d9afd78aa753758d9a1356dd816a22b73b

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -19,8 +19,8 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_E: *dest = cpu.ZF ? 1 : 0; break;
     case CC_BE: *dest = cpu.CF | cpu.ZF ? 1 : 0; break;
     case CC_S: *dest = cpu.SF ? 1 : 0; break;
-    case CC_L: *dest = cpu.SF == cpu.OF ? 1 : 0; break;
-    case CC_LE: *dest = cpu.SF == cpu.OF || cpu.ZF ? 1 : 0; break;
+    case CC_L: *dest = cpu.SF != cpu.OF ? 1 : 0; break;
+    case CC_LE: *dest = cpu.SF != cpu.OF || cpu.ZF ? 1 : 0; break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
