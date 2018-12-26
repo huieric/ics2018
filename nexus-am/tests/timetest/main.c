cd89199fde97c6d1b3795976bb7d064c5f2e9c1a
tracer-ics2018 <tracer@njuics.org>
2018-12-26 06:12:25 +0000

    >  run U201514582 shannonzhu Linux 3206989076e5 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux  06:12:25 up  3:32,  6 users,  load average: 0.44, 0.34, 0.15 366f556a51a579d41ac1f862c6b0f84c56d8627

--- nexus-am/tests/timetest/main.c
+++ nexus-am/tests/timetest/main.c
@@ -7,8 +7,9 @@ int main(){
   _ioe_init();
   int sec = 1;
   while (1) {
-    while(uptime() < 1000 * sec) printf("%d\n", uptime());
+    while(uptime() < 1000 * sec) ;
     get_timeofday(&rtc);
+    printf("\n");
     printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
     if (sec == 1) {
       printf("%d second).\n", sec);
