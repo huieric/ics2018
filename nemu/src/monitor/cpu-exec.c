f334d16d2c419ca979ebcd1e7a0cad9052bd9e68
tracer-ics2018 <tracer@njuics.org>
2018-12-28 22:52:34 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  22:52:34 up 2 days,  2:15,  6 users,  load average: 0.00, 0.00, 0.00 f5d6c1b66ae38f7f3a9631f2cff25a124781900

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -42,7 +42,7 @@ void cpu_exec(uint64_t n) {
     exec_wrapper(print_flag);
     nr_guest_instr_add(1);
   
-    if (cpu.eip == 0x100c03) {
+    if (cpu.eip == 0x100c27) {
       nemu_state = NEMU_STOP;
     }
 
