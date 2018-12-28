cf4b6b128f9942dc162f013092dd82488df01385
tracer-ics2018 <tracer@njuics.org>
2018-12-28 22:00:13 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  22:00:13 up 2 days,  1:23,  6 users,  load average: 0.00, 0.00, 0.00 9a2356be30475c103305c2843584d89d8c0060da

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -289,6 +289,7 @@ make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
   decoding.jmp_eip = id_dest->simm + *eip;
+  Log("%x %x", id_dest->simm, *eip);
 }
 
 make_DHelper(push_SI) {
