328b05c1904d3e1f7e30481a5bd49beca648df63
tracer-ics2018 <tracer@njuics.org>
2018-12-28 17:00:02 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  17:00:02 up 1 day, 20:23,  6 users,  load average: 0.00, 0.00, 0.00 59bc2a2df72cbd6c960c246b585d3a70c7525ef3

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,7 +11,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&cpu.eip);
   GateDesc gatedesc;
   vaddr_t addr = cpu.idtr.base + NO * sizeof(GateDesc);
-  Log("cpu.idtr.base=%x NO=%x addr=%x", cpu.idtr.base, NO, addr);
+  Log("cpu.idtr.base=%x NO=%x addr=%x eip=%x", cpu.idtr.base, NO, addr, cpu.eip);
   gatedesc.val = vaddr_read(addr, 4);
   decoding.jmp_eip = ((gatedesc.offset_31_16 << 16) | gatedesc.offset_15_0) + cpu.eip;
   rtl_j(decoding.jmp_eip);
