d246fefc3f3a0aaa3aa7b31e1700382c517ca73f
tracer-ics2018 <tracer@njuics.org>
2018-12-28 22:17:21 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  22:17:21 up 2 days,  1:40,  6 users,  load average: 0.00, 0.01, 0.00 909e287d713d6bcab3ae2d8b4aa97fd1ab21fe73

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -9,6 +9,7 @@ typedef void (*EHelper) (vaddr_t *);
 #include "cpu/decode.h"
 
 static inline uint32_t instr_fetch(vaddr_t *eip, int len) {
+  Log("%x %x", *eip, len);
   uint32_t instr = vaddr_read(*eip, len);
 #ifdef DEBUG
   uint8_t *p_instr = (void *)&instr;
