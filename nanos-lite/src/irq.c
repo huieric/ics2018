70051eebb3db32b670b03ba6935451a3372059f9
tracer-ics2018 <tracer@njuics.org>
2018-12-28 22:59:39 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  22:59:39 up 2 days,  2:23,  6 users,  load average: 0.00, 0.00, 0.00 c0cf18deda7f1d76a7c608fcb35cb565baf51266

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,7 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+    case _EVENT_YIELD: printf("do event yield\n");
     default: panic("Unhandled event ID = %d", e.event);
   }
 
