a3d851b0d5ebeb5b2f25c3975aeacb3a37282e80
tracer-ics2018 <tracer@njuics.org>
2018-12-29 03:31:21 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  03:31:21 up 2 days,  6:54,  6 users,  load average: 0.18, 0.06, 0.02 b93a42c0b8a6bfdc6e81e64cb847b4c588d1d070

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,14 +2,14 @@
 #include "syscall.h"
 
 void sys_yield(_Context* c);
-void sys_exit(_Context* c);
+void sys_exit(_Context* c, int code);
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   
   switch (a[0]) {
-    case 0: sys_exit(c); break;
+    case 0: sys_exit(c, 0); break;
     case 1: sys_yield(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
@@ -22,8 +22,7 @@ void sys_yield(_Context* c) {
   c->GPR1 = 0;
 }
 
-void sys_exit(_Context* c) {
-  Log("GPR1=%x GPR2=%x GPR3=%x GPR4=%x", c->GPR1, c->GPR2, c->GPR3, c->GPR4);
-  _halt(c->GPR2);
+void sys_exit(_Context* c, int code) {
+  _halt(code);
   c->GPR1 = 0;
 }
