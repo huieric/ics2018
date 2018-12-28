d5e98101e2e0c20a47faeeee09c18f99a6e9ecb2
tracer-ics2018 <tracer@njuics.org>
2018-12-28 21:57:28 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  21:57:28 up 2 days,  1:20,  6 users,  load average: 0.01, 0.01, 0.00 40f01098560e0650989e678e709e4693494d76fe

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -28,6 +28,7 @@ make_EHelper(call) {
   // the target address is calculated at the decode stage
   rtl_push(eip);
   rtl_j(decoding.jmp_eip);
+  Log("%x", decoding.jmp_eip);
 
   print_asm("call %x", decoding.jmp_eip);
 }
