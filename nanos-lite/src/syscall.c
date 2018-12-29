d1d978fd0eb7d381f48a3dca23ffcd40641515d2
tracer-ics2018 <tracer@njuics.org>
2018-12-29 22:16:51 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  22:16:51 up 3 days,  1:40,  6 users,  load average: 0.08, 0.02, 0.01 5d9e59498e39fe55168200223b66325964a66107

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,14 +3,16 @@
 
 void sys_yield(_Context* c);
 void sys_exit(_Context* c, int code);
+void sys_write(_Context* c);
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   
   switch (a[0]) {
-    case 0: sys_exit(c, 0); break;
-    case 1: sys_yield(c); break;
+    case SYS_exit: sys_exit(c, 0); break;
+    case SYS_yield: sys_yield(c); break;
+    case SYS_write: sys_write(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
@@ -25,4 +27,20 @@ void sys_yield(_Context* c) {
 void sys_exit(_Context* c, int code) {
   _halt(code);
   c->GPR1 = 0;
+}
+
+void sys_write(_Context* c) {
+  char* buf = (char*)c->GPR3;
+  size_t len = c->GPR4;
+  switch (c->GPR2) {
+    case 1: for (size_t i = 0; i < len; i++) {
+	      _putc(buf[i]);
+	    }
+	    c->GPR1 = 0;
+	    break;
+    case 2: assert(0);
+	    break;
+    default: assert(0);
+	     break;
+  }
 }
