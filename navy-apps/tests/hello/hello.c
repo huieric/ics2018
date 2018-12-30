2fe4dd9083b9ec2932cc5ea9d8bdeb9eba4dac6c
tracer-ics2018 <tracer@njuics.org>
2018-12-30 11:37:23 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  11:37:23 up 3 days, 15:00,  6 users,  load average: 0.00, 0.00, 0.00 6ed1172e7af5fed1e2c187462a19817d42bdb721

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -6,7 +6,7 @@ int main() {
   int i = 2;
   volatile int j = 0;
   while (1) {
-    write(1, "aaa\n", 256);
+    /*write(1, "aaa\n", 256);*/
     j ++;
     if (j == 10000) {
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
