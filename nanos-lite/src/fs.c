0eb4ff48beab1c6f680a2231c43c0ce0f9e902ca
tracer-ics2018 <tracer@njuics.org>
2018-12-30 18:33:35 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  18:33:35 up 3 days, 21:56,  5 users,  load average: 0.01, 0.03, 0.00 a0a557167a434b08cbbbd68d80193f48a70bffc

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,5 +1,8 @@
 #include "fs.h"
 
+size_t ramdisk_read(void* buf, size_t offset, size_t len);
+size_t ramdisk_write(const void* buf, size_t offset, size_t len);
+
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
@@ -36,4 +39,57 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+int fs_open(const char* pathname, int flags, int mode) {
+  for (int i = 0; i < NR_FILES; i++) {
+    if (strcmp(pathname, file_table[i].name) == 0) {
+      return i;
+    }
+  }
+  assert(0);
+  return -1;
+}
+
+size_t fs_read(int fd, void* buf, size_t len) {
+  Finfo f = file_table[fd];
+  if (f.size < f.open_offset + len) {
+    len = f.size - f.open_offset;
+  }
+  size_t real_len = ramdisk_read(buf, f.disk_offset + f.open_offset, len);
+  f.open_offset += real_len;
+  assert(0 <= f.open_offset && f.open_offset <= f.size);
+  return real_len;
+}
+
+size_t fs_write(int fd, const void* buf, size_t len) {
+  Finfo f = file_table[fd];
+  if (len > f.size - f.open_offset) {
+    len = f.size - f.open_offset;
+  }
+  size_t real_len = ramdisk_write(buf, f.disk_offset + f.open_offset, len);
+  f.open_offset += real_len;
+  assert(0 <= f.open_offset && f.open_offset <= f.size);
+  return real_len;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  enum { SEEK_SET, SEEK_CUR, SEEK_END, };
+  size_t* p = &file_table[fd].open_offset;
+  switch (whence) {
+    case SEEK_SET: *p = offset; break;
+    case SEEK_CUR: *p += offset; break;
+    case SEEK_END: *p = file_table[fd].size + offset; break;
+    default: assert(0);
+  }
+  assert(0 <= *p && *p <= file_table[fd].size);
+  return *p;
+}
+
+int fs_close(int fd) {
+  return 0;
+}
+
+size_t fs_filesz(int fd) {
+  return file_table[fd].size;
 }
