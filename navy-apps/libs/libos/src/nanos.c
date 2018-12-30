c2c05c28fa2d2a92c6513e042dc18585beb82f9e
tracer-ics2018 <tracer@njuics.org>
2018-12-30 10:36:06 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  10:36:06 up 3 days, 13:59,  4 users,  load average: 0.00, 0.00, 0.00 a615719b65c6865da2678bd1a76bccbe8a92a19

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -39,8 +39,8 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-  char buf[256];
-  sprintf(buf, "Good", 256);
+  char buf[256] = "Good\n";
+  /*sprintf(buf, "Good", 256);*/
   write(1, buf, 256);
   extern char end;
   static void* cur_brk = (void*)&end;
