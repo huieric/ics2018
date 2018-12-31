8a8144b54c3e699edba7484cbee504579694aa8a
tracer-ics2018 <tracer@njuics.org>
2018-12-31 14:34:45 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  14:34:45 up 4 days, 17:58,  3 users,  load average: 0.13, 0.05, 0.01 f2053941d5bcebde6b8994e2683fd6928da4d45b

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -28,8 +28,7 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  _syscall_(SYS_open, (intptr_t)path, flags, mode);
-  return 0;
+  return _syscall_(SYS_open, (intptr_t)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count){
