77f7aed3cac33e44a26a54dfcfe8e9bf146d039e
tracer-ics2018 <tracer@njuics.org>
2019-01-03 22:21:48 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  22:21:48 up 8 days,  1:45,  1 user,  load average: 0.00, 0.00, 0.00 99f7a3ee125050f59f9f908949a620b88f6f84bc

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -131,6 +131,7 @@ static void get_display_info() {
   screen_w = screen_h = 0;
   char buf[128], key[128], value[128], *delim;
   while (fgets(buf, 128, dispinfo)) {
+    printf("%s\n", buf);
     *(delim = strchr(buf, ':')) = '\0';
     sscanf(buf, "%s", key);
     sscanf(delim + 1, "%s", value);
