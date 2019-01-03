bae2db9e68c2f596994cd5ffca16af5a3ddd1dcc
tracer-ics2018 <tracer@njuics.org>
2019-01-03 23:13:43 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  23:13:43 up 8 days,  2:37,  1 user,  load average: 0.07, 0.04, 0.01 2cfb25c28a3c3915af3f8acc8f58cdd5fb5c7385

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -17,6 +17,7 @@ size_t invalid_read(void *buf, size_t offset, size_t len);
 size_t invalid_write(const void *buf, size_t offset, size_t len);
 size_t dispinfo_read(void* buf, size_t offset, size_t len);
 size_t fb_write(const void* buf, size_t offset, size_t len);
+size_t serial_write(const void* buf, size_t offset, size_t len);
 int fs_open(const char* pathname, int flags, int mode);
 
 typedef struct {
@@ -33,8 +34,8 @@ enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, 0, invalid_read, invalid_write},
-  {"stderr", 0, 0, 0, invalid_read, invalid_write},
+  {"stdout", 0, 0, 0, invalid_read, serial_write},
+  {"stderr", 0, 0, 0, invalid_read, serial_write},
   {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
   {"/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
