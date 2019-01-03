6a2e31b0c6a7238da50b7398a47acb1dc891d23f
tracer-ics2018 <tracer@njuics.org>
2019-01-04 01:12:24 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  01:12:24 up 8 days,  4:35,  1 user,  load average: 0.19, 0.08, 0.02 ad3f154575319dda529842fe7f1b994afb56eec1

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,7 +29,6 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   if (offset + len > strlen(dispinfo)) {
     len = strlen(dispinfo) - offset;
   }
-  Log("offset=%u len=%u", offset, len);
   for (int i = 0; i < len; i++) {
     ((char*)buf)[i] = dispinfo[offset + i];
   }
@@ -37,11 +36,10 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  Log("offset=%u len=%u", offset, len);
   int x = offset % screen_width();
   int y = offset / screen_width();
-  int w = len / (screen_width() - x);
-  int h = len / (screen_height() - y);
+  int w = len;
+  int h = 1;
   draw_rect((uint32_t*)buf, x, y, w, h);
   return len;
 }
