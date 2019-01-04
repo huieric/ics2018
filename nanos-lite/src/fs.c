d0046899e7dd6780e723e012f7379a9a43931ec8
tracer-ics2018 <tracer@njuics.org>
2019-01-04 23:39:09 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  23:39:09 up 9 days,  3:02,  4 users,  load average: 0.08, 0.05, 0.01 183c20dcd57168a03f12be054f14b1efdd16ff0c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -33,6 +33,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
   {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
   {"/dev/events", 0, 0, 0, events_read, invalid_write},
+  {"/dev/tty", 0, 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
