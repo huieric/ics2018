9897a5da9cfe17f681a035891a3fb94fe239c785
tracer-ics2018 <tracer@njuics.org>
2019-01-04 10:36:26 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  10:36:26 up 8 days, 13:59,  1 user,  load average: 0.08, 0.02, 0.00 a82836a78e68d8f2ef081ded64082c3be6892e2

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -9,10 +9,8 @@ int main() {
   assert(bmp->pixels);
   NDL_OpenDisplay(bmp->w, bmp->h);
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
-  printf("w=%d h=%d\n", bmp->w, bmp->h);
   NDL_Render();
   NDL_CloseDisplay();
-  printf("done\n");
   while (1);
   return 0;
 }
