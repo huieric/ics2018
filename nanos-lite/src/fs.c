b18d729649c0a20c37fe0d1b5f3299feee01c83e
tracer-ics2018 <tracer@njuics.org>
2019-01-04 21:21:02 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  21:21:02 up 9 days, 44 min,  2 users,  load average: 0.00, 0.00, 0.00 ae2cfdabd34c57faa799eeaed458f20728288431

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -54,6 +54,7 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 }
 
 int fs_open(const char* pathname, int flags, int mode) {
+  Log("pathname=%s", pathname);
   for (int i = 0; i < NR_FILES; i++) {
     if (strcmp(pathname, file_table[i].name) == 0) {
       file_table[i].open_offset = 0;
