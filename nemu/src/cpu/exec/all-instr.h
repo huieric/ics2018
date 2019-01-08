fee3709fb68b957419d609ca6920910a6f2fea40
tracer-ics2018 <tracer@njuics.org>
2019-01-08 21:58:38 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  21:58:38 up 13 days,  1:22,  2 users,  load average: 0.07, 0.02, 0.00 92d365f22fee09ce92661d501e65a29e33b69fd9

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -53,3 +53,5 @@ make_EHelper(int);
 make_EHelper(pusha);
 make_EHelper(popa);
 make_EHelper(iret);
+make_EHelper(mov_r2cr);
+make_EHelper(mov_cr2r);
--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -29,13 +29,21 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-  TODO();
+  switch (id_dest->reg) {
+    case R_EAX: cpu.CR0 = id_src->val; break;
+    case R_EBX: cpu.CR3 = id_src->val; break;
+    default: assert(0);
+  }
 
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  TODO();
+  switch (id_src->reg) {
+    case R_EAX: operand_write(id_dest, &cpu.CR0);
+    case R_EBX: operand_write(id_dest, &cpu.CR3);
+    default: assert(0);
+  }
 
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
