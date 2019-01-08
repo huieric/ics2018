82f990b7bed26b2b5e27a8ce207ee7fe536314ca
tracer-ics2018 <tracer@njuics.org>
2019-01-08 23:04:51 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  23:04:51 up 13 days,  2:28,  2 users,  load average: 0.00, 0.00, 0.00 9441d5ec939ca0bbf802a79c2cebb64e59e9a863

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -85,7 +85,7 @@ void vaddr_write(vaddr_t addr, uint32_t data, int len) {
 
 paddr_t page_translate(vaddr_t vaddr) {
   PDE* pdir = (PDE*)(uintptr_t)PTE_ADDR(cpu.CR3);
-  Log("PDX(vaddr)=%dpdir=0x%lx", PDX(vaddr), (uintptr_t)PTE_ADDR(cpu.CR3));
+  Log("PDX(vaddr)=%d pdir=0x%lx eip=0x%x", PDX(vaddr), (uintptr_t)PTE_ADDR(cpu.CR3), cpu.eip);
   assert(pdir[PDX(vaddr)] & PTE_P);
   PTE* ptab = (PTE*)(uintptr_t)PTE_ADDR(pdir[PDX(vaddr)]);
   assert(ptab[PTX(vaddr)] & PTE_P);
