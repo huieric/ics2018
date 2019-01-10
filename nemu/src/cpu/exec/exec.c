f1d8067e4ca0935116792d7d8d082cc5c3240465
tracer-ics2018 <tracer@njuics.org>
2019-01-10 14:10:27 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  14:10:27 up 14 days, 17:33,  2 users,  load average: 0.00, 0.00, 0.00 eac3f9573569841bd85b79459fd7c0fd1ce8674a

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -226,7 +226,7 @@ static inline void update_eip(void) {
 
 void exec_wrapper(bool print_flag) {
   vaddr_t ori_eip = cpu.eip;
-  if (ori_eip == 0x101000 || ori_eip == 0x101001 || ori_eip == 0x101002 || ori_eip == 0x101003 || ori_eip == 0x101004) {
+  if ((ori_eip & ~0xf) == 0x101000) {
     Log("eip=0x%x", ori_eip);
   }
 
