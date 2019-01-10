3fcdd12c42b69db2a770cbbf26a3f67ca2fba33f
tracer-ics2018 <tracer@njuics.org>
2019-01-10 11:17:00 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  11:17:00 up 14 days, 14:40,  2 users,  load average: 0.00, 0.00, 0.00 47a51409b0032c0ec2e256014f1d58a04be42fd8

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -87,12 +87,12 @@ void vaddr_write(vaddr_t addr, uint32_t data, int len) {
 paddr_t page_translate(vaddr_t vaddr) {
   paddr_t pdir = PTE_ADDR(cpu.CR3);
   if (!(PTE_DATA(pdir, PDX(vaddr)) & PTE_P)) {
-    Log("vaddr=0x%x pdir=0x%x pdir[PTX(va)]=0x%x", vaddr, pdir, PTE_DATA(pdir, PDX(vaddr)));
+    Log("eip=0x%x vaddr=0x%x pdir=0x%x pdir[PTX(va)]=0x%x", cpu.eip, vaddr, pdir, PTE_DATA(pdir, PDX(vaddr)));
   }
   assert(PTE_DATA(pdir, PDX(vaddr)) & PTE_P);
   paddr_t ptab = PTE_ADDR(PTE_DATA(pdir, PDX(vaddr)));
   if (!(PTE_DATA(ptab, PTX(vaddr)) & PTE_P)) {
-    Log("vaddr=0x%x pdir=0x%x ptab=0x%x ptab[PTX(va)]=0x%x", vaddr, pdir, ptab, PTE_DATA(ptab, PTX(vaddr)));
+    Log("eip=0x%x vaddr=0x%x pdir=0x%x ptab=0x%x ptab[PTX(va)]=0x%x", cpu.eip, vaddr, pdir, ptab, PTE_DATA(ptab, PTX(vaddr)));
   }
   assert(PTE_DATA(ptab, PTX(vaddr)) & PTE_P);
   return (PTE_ADDR(PTE_DATA(ptab, PTX(vaddr))) + OFF(vaddr));
