f0814e92ee227beb6d5c504323b45b925f46aecf
tracer-ics2018 <tracer@njuics.org>
2019-01-10 10:49:14 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  10:49:14 up 14 days, 14:12,  2 users,  load average: 0.00, 0.00, 0.00 ddc433c9f3b4e1a7b640fcd5e88d5bea1076bb80

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,8 +24,8 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[0], (void*)hello_fun);
-  // context_uload(&pcb[0], "/bin/hello");
+  // context_kload(&pcb[0], (void*)hello_fun);
+  context_uload(&pcb[0], "/bin/hello");
   // context_uload(&pcb[1], "/bin/bmp");
   switch_boot_pcb();
 }
