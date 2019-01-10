1a11e71f2f762c5c6baf38851641f96d3e61d714
tracer-ics2018 <tracer@njuics.org>
2019-01-10 13:44:36 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  13:44:36 up 14 days, 17:07,  2 users,  load average: 0.00, 0.00, 0.00 811971879a099012ed0e4f3173185b3c8b3736ec

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -1,7 +1,8 @@
 #include "cpu/exec.h"
 #include "cpu/rtl.h"
 
-void load_addr(vaddr_t *eip, ModR_M *m, Operand *rm) {
+void load_addr(vaddr_t *eip, ModR_M *m, Operand *rm)
+{
   assert(m->mod != 3);
 
   int32_t disp = 0;
@@ -9,38 +10,59 @@ void load_addr(vaddr_t *eip, ModR_M *m, Operand *rm) {
   int base_reg = -1, index_reg = -1, scale = 0;
   rtl_li(&t0, 0);
 
-  if (m->R_M == R_ESP) {
+  if (m->R_M == R_ESP)
+  {
     SIB s;
     s.val = instr_fetch(eip, 1);
     base_reg = s.base;
     scale = s.ss;
 
-    if (s.index != R_ESP) { index_reg = s.index; }
+    if (s.index != R_ESP)
+    {
+      index_reg = s.index;
+    }
   }
-  else {
+  else
+  {
     /* no SIB */
     base_reg = m->R_M;
   }
 
-  if (m->mod == 0) {
-    if (base_reg == R_EBP) { base_reg = -1; }
-    else { disp_size = 0; }
+  if (m->mod == 0)
+  {
+    if (base_reg == R_EBP)
+    {
+      base_reg = -1;
+    }
+    else
+    {
+      disp_size = 0;
+    }
   }
-  else if (m->mod == 1) { disp_size = 1; }
+  else if (m->mod == 1)
+  {
+    disp_size = 1;
+  }
 
-  if (disp_size != 0) {
+  if (disp_size != 0)
+  {
     /* has disp */
     disp = instr_fetch(eip, disp_size);
-    if (disp_size == 1) { disp = (int8_t)disp; }
+    if (disp_size == 1)
+    {
+      disp = (int8_t)disp;
+    }
 
     rtl_addi(&t0, &t0, disp);
   }
 
-  if (base_reg != -1) {
+  if (base_reg != -1)
+  {
     rtl_add(&t0, &t0, &reg_l(base_reg));
   }
 
-  if (index_reg != -1) {
+  if (index_reg != -1)
+  {
     rtl_shli(&t1, &reg_l(index_reg), scale);
     rtl_add(&t0, &t0, &t1);
   }
@@ -51,26 +73,40 @@ void load_addr(vaddr_t *eip, ModR_M *m, Operand *rm) {
   char base_buf[8];
   char index_buf[8];
 
-  if (disp_size != 0) {
+  if (disp_size != 0)
+  {
     /* has disp */
     sprintf(disp_buf, "%s%#x", (disp < 0 ? "-" : ""), (disp < 0 ? -disp : disp));
   }
-  else { disp_buf[0] = '\0'; }
+  else
+  {
+    disp_buf[0] = '\0';
+  }
 
-  if (base_reg == -1) { base_buf[0] = '\0'; }
-  else { 
+  if (base_reg == -1)
+  {
+    base_buf[0] = '\0';
+  }
+  else
+  {
     sprintf(base_buf, "%%%s", reg_name(base_reg, 4));
   }
 
-  if (index_reg == -1) { index_buf[0] = '\0'; }
-  else { 
+  if (index_reg == -1)
+  {
+    index_buf[0] = '\0';
+  }
+  else
+  {
     sprintf(index_buf, ",%%%s,%d", reg_name(index_reg, 4), 1 << scale);
   }
 
-  if (base_reg == -1 && index_reg == -1) {
+  if (base_reg == -1 && index_reg == -1)
+  {
     sprintf(rm->str, "%s", disp_buf);
   }
-  else {
+  else
+  {
     sprintf(rm->str, "%s(%s%s)", disp_buf, base_buf, index_buf);
   }
 #endif
@@ -78,15 +114,21 @@ void load_addr(vaddr_t *eip, ModR_M *m, Operand *rm) {
   rm->type = OP_TYPE_MEM;
 }
 
-void read_ModR_M(vaddr_t *eip, Operand *rm, bool load_rm_val, Operand *reg, bool load_reg_val) {
-  Log("eip=0x%x", *eip);
+void read_ModR_M(vaddr_t *eip, Operand *rm, bool load_rm_val, Operand *reg, bool load_reg_val)
+{
+  if (cpu.eip == 0x101001)
+  {
+    Log("eip=0x%x", *eip);
+  }
   ModR_M m;
   m.val = instr_fetch(eip, 1);
   decoding.ext_opcode = m.opcode;
-  if (reg != NULL) {
+  if (reg != NULL)
+  {
     reg->type = OP_TYPE_REG;
     reg->reg = m.reg;
-    if (load_reg_val) {
+    if (load_reg_val)
+    {
       rtl_lr(&reg->val, reg->reg, reg->width);
     }
 
@@ -95,10 +137,12 @@ void read_ModR_M(vaddr_t *eip, Operand *rm, bool load_rm_val, Operand *reg, bool
 #endif
   }
 
-  if (m.mod == 3) {
+  if (m.mod == 3)
+  {
     rm->type = OP_TYPE_REG;
     rm->reg = m.R_M;
-    if (load_rm_val) {
+    if (load_rm_val)
+    {
       rtl_lr(&rm->val, m.R_M, rm->width);
     }
 
@@ -106,12 +150,15 @@ void read_ModR_M(vaddr_t *eip, Operand *rm, bool load_rm_val, Operand *reg, bool
     sprintf(rm->str, "%%%s", reg_name(m.R_M, rm->width));
 #endif
   }
-  else {
+  else
+  {
     load_addr(eip, &m, rm);
-    if (cpu.eip == 0x101001) {
+    if (cpu.eip == 0x101001)
+    {
       Log("eip=0x%x m=0x%x rm->addr=0x%x", *eip, m.val, rm->addr);
     }
-    if (load_rm_val) {
+    if (load_rm_val)
+    {
       rtl_lm(&rm->val, &rm->addr, rm->width);
     }
   }
