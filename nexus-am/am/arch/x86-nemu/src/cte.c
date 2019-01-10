80a1fe8ae8b37dc5659ebbfce77b17b6851bfcd3
tracer-ics2018 <tracer@njuics.org>
2019-01-10 10:57:55 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  10:57:55 up 14 days, 14:21,  2 users,  load average: 0.00, 0.01, 0.00 a541196ce6928d79914c414c615f4f87520b834f

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -57,7 +57,6 @@ _Context *_kcontext(_Area stack, void (*entry)(void *), void *arg) {
   cp->eip = (uintptr_t)entry;
   cp->cs = 0x8;
   cp->esp = (uintptr_t)((void*)cp + sizeof(struct _Protect*) + 3 * sizeof(uintptr_t));
-  get_cur_as(cp);
   return cp;
 }
 
