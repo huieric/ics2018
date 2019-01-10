607c79916ea5ad55b63be54766f47e538ede6f1c
tracer-ics2018 <tracer@njuics.org>
2019-01-10 13:41:33 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  13:41:33 up 14 days, 17:04,  2 users,  load average: 0.00, 0.00, 0.00 a74e23fad94bfdca4e199cfe1d597463bfa8a40d

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -107,6 +107,9 @@ void read_ModR_M(vaddr_t *eip, Operand *rm, bool load_rm_val, Operand *reg, bool
   }
   else {
     load_addr(eip, &m, rm);
+    if (cpu.eip == 0x101001) {
+      Log("eip=0x%x m=0x%x rm->addr=0x%x", *eip, m.val, rm->addr);
+    }
     if (load_rm_val) {
       rtl_lm(&rm->val, &rm->addr, rm->width);
     }
