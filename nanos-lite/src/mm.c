9812550e8f40fde54d8d4902424ba435a31d099a
tracer-ics2018 <tracer@njuics.org>
2019-01-12 11:41:44 +0800

    >  run U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  11:41:44 up 16 days, 15:05,  3 users,  load average: 0.08, 0.02, 0.01 fa0ef63eda822d0c798d36211d9a2cb6923140ec

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,11 +16,9 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t new_brk) {
-  static int i = 0;  
-  if (i == 0) {
-    current->max_brk = current->cur_brk = new_brk;
+  if (current->max_brk == 0) {
+    current->max_brk = current->cur_brk = new_brk; //init in first call
   }
-  i++;
   uintptr_t brk = current->max_brk;
   for (; brk < new_brk; brk += PGSIZE) {
     void* pa = new_page(1);
