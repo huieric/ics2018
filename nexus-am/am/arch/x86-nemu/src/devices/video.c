b4903bec670a286242d438b14b25bc72b830c215
tracer-ics2018 <tracer@njuics.org>
2019-01-16 17:06:16 +0800

    >  compile U201514582 shannonzhu Linux VM-0-7-debian 4.9.0-3-amd64 #1 SMP Debian 4.9.30-2 (2017-06-12) x86_64 GNU/Linux  17:06:16 up 20 days, 20:29,  2 users,  load average: 0.00, 0.02, 0.00 abc749acb96367e5c900d5cd4b700c4865c87576

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -5,40 +5,47 @@
 
 #define SCREEN_PORT 0x100
 
-static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
+static uint32_t *const fb __attribute__((used)) = (uint32_t *)0x40000;
 
-size_t video_read(uintptr_t reg, void *buf, size_t size) {
+size_t video_read(uintptr_t reg, void *buf, size_t size)
+{
   uint32_t video_info = inl(SCREEN_PORT);
-  switch (reg) {
-    case _DEVREG_VIDEO_INFO: {
-      _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = (video_info >> 16) & 0xffff;
-      info->height =  video_info & 0xffff;
-      return sizeof(_VideoInfoReg);
-    }
+  switch (reg)
+  {
+  case _DEVREG_VIDEO_INFO:
+  {
+    _VideoInfoReg *info = (_VideoInfoReg *)buf;
+    info->width = (video_info >> 16) & 0xffff;
+    info->height = video_info & 0xffff;
+    return sizeof(_VideoInfoReg);
+  }
   }
   return 0;
 }
 
-size_t video_write(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_FBCTL: {
-      _FBCtlReg *ctl = (_FBCtlReg *)buf;
-      for (int i = 0; i < ctl->h; i++) {
-	for (int j = 0; j < ctl->w; j++) {
-	  /*assert((ctl->y + j) * screen_width() + (ctl->x + i) < screen_width() * screen_height());*/
-	  fb[(ctl->y + i) * screen_width() + (ctl->x + j)] = ctl->pixels[i * ctl->w + j];
-	}
-      }
-	  	  
-      if (ctl->sync) {
-        // do nothing, hardware syncs.
-      }
-      return sizeof(_FBCtlReg);
+size_t video_write(uintptr_t reg, void *buf, size_t size)
+{
+  switch (reg)
+  {
+  case _DEVREG_VIDEO_FBCTL:
+  {
+    _FBCtlReg *ctl = (_FBCtlReg *)buf;
+    int width = screen_width();
+    for (int i = 0; i < ctl->h; i++)
+    {
+      memcpy(fb + (ctl->y + i) * width + ctl->x, ctl->pixels + i * ctl->w, ctl->w * 4);
     }
+
+    if (ctl->sync)
+    {
+      // do nothing, hardware syncs.
+    }
+    return sizeof(_FBCtlReg);
+  }
   }
   return 0;
 }
 
-void vga_init() {
+void vga_init()
+{
 }
